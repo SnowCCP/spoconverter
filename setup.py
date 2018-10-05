3c21610e3110b356d72c4e9af8f2decd2150b4f8
Pelayo Calleja Coto <>
2018-10-05 23:36:45 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='spoconverter',
+    version='0.1',
+    packages=['spoconverter'],
+    url='',
+    license='MIT',
+    author='SnowCCP',
+    author_email='',
+    description='',
+    install_requires=[
+        "spotipy",
+        "youtube-dl",
+        "eyeD3"
+    ]
+)
